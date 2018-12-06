963b945600c35dd948f6b19682ba6b520de6a9bc
ANDREI SIMION <arse1983@ANDREIs-MacBook-Pro-2.local>
2018-12-06 16:38:42 -0500

    Added problem 8, count univalue subtrees, which is faster.

--- problem8UnivariateTree.cpp
+++ problem8UnivariateTree.cpp
@@ -1,52 +1,60 @@
-/**
  * Definition for a binary tree node.
  * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
- * };
+   *     int val;
+   *     TreeNode *left;
+   *     TreeNode *right;
+   *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+   * };
  */
-class Solution {
-public:
+ class Solution {
+ public:
     
-  bool isUnivariate(TreeNode * root) {
-    int val = root->val;
+   bool isUnivariate(TreeNode * root) {
+     int val = root->val;
         
-    bool left = true;
-    bool right = true;
+     bool left = true;
+     bool right = true;
         
-    if(root->left) {
-      if(root->left->val != val) {
-	return false;
-      }
-      left = isUnivariate(root->left);
-    }
+     if(root->left) {
+       if(root->left->val != val) {
+	 return false;
+       }
+       left = isUnivariate(root->left);
+     }
         
-    if(root->right) {
-      if(root->right->val != val) {
-	return false;
-      }
-      right = isUnivariate(root->right);
-    }
+     if(root->right) {
+       if(root->right->val != val) {
+	 return false;
+       }
+       right = isUnivariate(root->right);
+     }
         
-    if(!root->left && !root->right) {
-      return true;
-    }
+     if(!root->left && !root->right) {
+       return true;
+     }
         
-    return left && right;
-  }
+     return left && right;
+   }
     
-  int countUnivalSubtrees(TreeNode* root) {
+   int countUnivalSubtrees(TreeNode* root, bool is_univariate) {
         
-    if(!root){
-      return 0;
-    }
+     if(!root){
+       return 0;
+     }
         
-    if(isUnivariate(root)) {
-      return 1 + countUnivalSubtrees(root->left) + countUnivalSubtrees(root->right);
-    }
+     if(!is_univariate) {
+       is_univariate = isUnivariate(root);
+     }
         
-    return countUnivalSubtrees(root->left) + countUnivalSubtrees(root->right);
-  }
-};
+     if(is_univariate) {
+       return 1 + countUnivalSubtrees(root->left, is_univariate) + countUnivalSubtrees(root->right, is_univariate);
+     }
+        
+     return countUnivalSubtrees(root->left, is_univariate) + countUnivalSubtrees(root->right, is_univariate);
+   }
+    
+   int countUnivalSubtrees(TreeNode* root) {
+     return countUnivalSubtrees(root, false);
+   }
+    
+ };
